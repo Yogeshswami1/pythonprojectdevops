7227e3c4f7ff86354f3f037246f0fdc8bb848c32
Yogesh <yogeshkumar86077@gmail.com>
2025-09-18 21:04:34 +0530

    third changes

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def add_employee():
     db.session.commit()
     return jsonify({'message': 'Employee added successfully!'}), 201
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
